c8a5ccd9a5c9cf47a6151376579d561f045519db
tobias.tlaskal <tobias.tlaskal@getready.cz>
2025-01-15 23:27:55 +0100

    MaterialTheme úpravy

--- app/src/main/java/cz/tlaskal/inventurapp/ui/theme/Color.kt
+++ app/src/main/java/cz/tlaskal/inventurapp/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package cz.tlaskal.inventurapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Orange80 = Color(0xFFE5B83E)
+val OrangeGrey80 = Color(0xFFC0B59F)
+val Brown80 = Color(0xFF834F33)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Orange40 = Color(0xFFCE8D14)
+val OrangeGrey40 = Color(0xFF635438)
+val Brown40 = Color(0xFF6A4400)
+
+//val Purple80 = Color(0xFFD0BCFF)
+//val PurpleGrey80 = Color(0xFFCCC2DC)
+//val Pink80 = Color(0xFFEFB8C8)
+//
+//val Purple40 = Color(0xFF6650a4)
+//val PurpleGrey40 = Color(0xFF625b71)
+//val Pink40 = Color(0xFF7D5260)
--- app/src/main/java/cz/tlaskal/inventurapp/ui/theme/Theme.kt
+++ app/src/main/java/cz/tlaskal/inventurapp/ui/theme/Theme.kt
@@ -11,32 +11,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Orange80,
+    secondary = OrangeGrey80,
+    tertiary = Brown80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Orange40,
+    secondary = OrangeGrey40,
+    tertiary = Brown40
 )
 
 @Composable
 fun InventurAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
