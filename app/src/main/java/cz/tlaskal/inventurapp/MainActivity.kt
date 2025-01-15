a33270e67d7fc708f861d325e74c50195d6ffd63
tobias.tlaskal <tobias.tlaskal@getready.cz>
2025-01-15 23:32:03 +0100

    MainActivity spouští NavHost

--- app/src/main/java/cz/tlaskal/inventurapp/MainActivity.kt
+++ app/src/main/java/cz/tlaskal/inventurapp/MainActivity.kt
@@ -2,71 +2,44 @@ package cz.tlaskal.inventurapp
 
 import android.content.Context
 import android.os.Bundle
-import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Search
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModelProvider
-import cz.tlaskal.inventurapp.barcodehandler.BarcodeProcessor
-import cz.tlaskal.inventurapp.ui.ScannerViewModel
-import cz.tlaskal.inventurapp.ui.components.ScannerTextInput
+import androidx.lifecycle.viewmodel.compose.viewModel
+import cz.tlaskal.inventurapp.ui.components.ScannerTextViewModel
 import cz.tlaskal.inventurapp.ui.theme.InventurAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val viewModel = ViewModelProvider(this).get(ScannerViewModel::class)
         enableEdgeToEdge()
         setContent {
             InventurAppTheme {
-                Scaffold(modifier = Modifier
-                    .fillMaxSize()
-                    .padding(18.dp)
-                ) { innerPadding ->
-                    Column {
-                        Greeting(
-                                name = "Tobias",
-                                modifier = Modifier.padding(innerPadding)
-                        )
-                        if (viewModel.barcode.value != "") {
-                                Text("Naskenovaný kód: ${viewModel.barcode.value}")
-                        }
-                        Row(verticalAlignment = Alignment.CenterVertically) {
-                            ScannerTextInput(viewModel = viewModel, this@MainActivity,"kodik objektu")
-                        }
-                    }
-                }
+                InventurApp()
             }
+            //MainView(viewModel, this)
         }
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainView(viewModel: ScannerTextViewModel, context: Context){
+
+}
+
+@Preview
+@Composable
+fun MainViewPreview(viewModel: ScannerTextViewModel = viewModel()){
+    MainView(viewModel, LocalContext.current)
+}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
