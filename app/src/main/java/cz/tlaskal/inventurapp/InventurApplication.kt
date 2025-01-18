307341ff25cb05bdc02354d5f16c0c0b1ea16af6
tobias.tlaskal <tobias.tlaskal@getready.cz>
2025-01-18 19:20:20 +0100

    DI refactoring

--- app/src/main/java/cz/tlaskal/inventurapp/InventurApplication.kt
+++ app/src/main/java/cz/tlaskal/inventurapp/InventurApplication.kt
@@ -1,8 +1,8 @@
 package cz.tlaskal.inventurapp
 
 import android.app.Application
-import cz.tlaskal.inventurapp.data.AppContainer
-import cz.tlaskal.inventurapp.data.AppDataContainer
+import cz.tlaskal.inventurapp.util.AppContainer
+import cz.tlaskal.inventurapp.util.AppDataContainer
 
 class InventurApplication : Application() {
     lateinit var container: AppContainer
--- app/src/main/java/cz/tlaskal/inventurapp/util/AppContainer.kt
+++ app/src/main/java/cz/tlaskal/inventurapp/util/AppContainer.kt
@@ -1,6 +1,9 @@
-package cz.tlaskal.inventurapp.data
+package cz.tlaskal.inventurapp.util
 
 import android.content.Context
+import cz.tlaskal.inventurapp.data.InventoryDatabase
+import cz.tlaskal.inventurapp.data.ItemsRepository
+import cz.tlaskal.inventurapp.data.LocalItemsRepository
 
 //manuální DI - AppContainer je dostupný ve všech třídách,
 // musí být instancován v InventurApp Application třídě
@@ -12,7 +15,7 @@ interface AppContainer {
 class AppDataContainer(private val context: Context) : AppContainer{
 
     override val itemsRepository: ItemsRepository by lazy {
-        LocalItemsRepository(InventoryDatabase.getDatabase(context).itemDao())
+        LocalItemsRepository(InventoryDatabase.Companion.getDatabase(context).itemDao())
     }
 
 }
