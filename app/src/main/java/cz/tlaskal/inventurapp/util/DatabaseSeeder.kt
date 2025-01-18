a1dd9d4a93231076d739c9ed6aa72a7fdb412c09
tobias.tlaskal <tobias.tlaskal@getready.cz>
2025-01-18 19:20:35 +0100

    Seeder update

--- app/src/main/java/cz/tlaskal/inventurapp/util/DatabaseSeeder.kt
+++ app/src/main/java/cz/tlaskal/inventurapp/util/DatabaseSeeder.kt
@@ -1,11 +1,22 @@
 package cz.tlaskal.inventurapp.util
 
+import android.app.Activity
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.CreationExtras
 import cz.tlaskal.inventurapp.InventurApplication
+import cz.tlaskal.inventurapp.data.Item
 import cz.tlaskal.inventurapp.data.ItemsRepository
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import java.util.Date
 
 class DatabaseSeeder(private val itemsRepository: ItemsRepository) {
-    fun seedDatabase() {
-        val itemsRepository = null
+    suspend fun seedDatabase(){
+        itemsRepository.insertItem(Item("5", "Pocitac", "Hezky novy pocitac", Date.UTC(2022,2,11,8,20, 36), false))
+        itemsRepository.insertItem(Item("6", "Penalek", "Hezky stary penal", Date.UTC(2018,5,20,8,20, 36), false))
+    }
+
+    suspend fun nukeItems(){
+        itemsRepository.nukeItems()
     }
 }
