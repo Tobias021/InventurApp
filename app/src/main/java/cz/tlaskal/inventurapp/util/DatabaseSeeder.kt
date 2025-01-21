6ab5e8849e58f932a51ee4c2692a7425d2909128
tobias.tlaskal <tobias.tlaskal@getready.cz>
2025-01-22 00:50:30 +0100

    seeder added rows

--- app/src/main/java/cz/tlaskal/inventurapp/util/DatabaseSeeder.kt
+++ app/src/main/java/cz/tlaskal/inventurapp/util/DatabaseSeeder.kt
@@ -14,6 +14,11 @@ class DatabaseSeeder(private val itemsRepository: ItemsRepository) {
     suspend fun seedDatabase(){
         itemsRepository.insertItem(Item("5", "Pocitac", "Hezky novy pocitac", Date.UTC(2022,2,11,8,20, 36), false))
         itemsRepository.insertItem(Item("6", "Penalek", "Hezky stary penal", Date.UTC(2018,5,20,8,20, 36), false))
+        itemsRepository.insertItem(Item("7", "Penalek", "Hezky stary penal", Date.UTC(2018,5,20,8,20, 36), false))
+        itemsRepository.insertItem(Item("8", "Penalek", "Hezky stary penal", Date.UTC(2018,5,20,8,20, 36), false))
+        itemsRepository.insertItem(Item("9", "Penalek", "Hezky stary penal", Date.UTC(2018,5,20,8,20, 36), false))
+        itemsRepository.insertItem(Item("10", "Penalek", "Hezky stary penal", Date.UTC(2018,5,20,8,20, 36), false))
+        itemsRepository.insertItem(Item("11", "Penalek", "Hezky stary penal", Date.UTC(2018,5,20,8,20, 36), false))
     }
 
     suspend fun nukeItems(){
