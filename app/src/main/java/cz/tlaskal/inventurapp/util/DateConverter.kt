c7bbefadd4ecfad10a8a16a9f657e52712306e23
tobias.tlaskal <tobias.tlaskal@getready.cz>
2025-01-23 21:41:22 +0100

    DateConverter fix

--- app/src/main/java/cz/tlaskal/inventurapp/util/DateConverter.kt
+++ app/src/main/java/cz/tlaskal/inventurapp/util/DateConverter.kt
@@ -1,11 +1,10 @@
 package cz.tlaskal.inventurapp.util
 
+import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Locale
 
-fun dateToTimestamp(date: Date): Long{
-    return date.time
-}
-
-fun timestampToDate(timestamp: Long): Date{
-    return Date(timestamp, )
+fun timestampToString(timestamp: Long): String {
+    val formatter = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+    return formatter.format(Date(timestamp))
 }
