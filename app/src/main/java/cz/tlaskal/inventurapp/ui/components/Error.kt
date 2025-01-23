54879eee5de532280732be755b8661f5f55d34b4
tobias.tlaskal <tobias.tlaskal@getready.cz>
2025-01-23 21:41:33 +0100

    Add error composable

---
+++ app/src/main/java/cz/tlaskal/inventurapp/ui/components/Error.kt
@@ -0,0 +1,44 @@
+package cz.tlaskal.inventurapp.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import java.sql.Statement
+
+/**
+ * Simple Error panel showing a message when passed a nan-null string [message].
+ *
+ * @param message: Error message to be shown.
+ * @param onClick: Lambda to be called when the message is clicked.
+ * @author Tobiáš Tláskal
+ */
+
+@Composable
+fun Error(message: String?, onClick: () -> Unit){
+    if (message != null) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(10.dp, 50.dp)
+                .background(MaterialTheme.colorScheme.error)
+                .clickable { onClick.invoke() }
+
+        ) {
+            Text(
+                text = message,
+                modifier = Modifier.padding(5.dp),
+                color = Color.Black
+            )
+        }
+    }
+}
