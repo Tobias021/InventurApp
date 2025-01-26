ef4955989954f08dee5fb81c5d1986ad46ef6d0e
tobias.tlaskal <tobias.tlaskal@getready.cz>
2025-01-27 00:53:21 +0100

    refactoring

--- app/src/main/java/cz/tlaskal/inventurapp/ui/components/Message.kt
+++ app/src/main/java/cz/tlaskal/inventurapp/ui/components/Message.kt
@@ -1,5 +1,6 @@
 package cz.tlaskal.inventurapp.ui.components
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -7,19 +8,29 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
+import cz.tlaskal.inventurapp.ui.theme.InventurAppTheme
 
 /**
- * Simple Error panel showing a message when passed a nan-null string [message].
+ * Simple Message panel showing a message when passed a nan-null string [message].
  *
- * @param message: Error message to be shown.
+ * @param message: Message to be shown.
  * @param onClick: Lambda to be called when the message is clicked.
  * @author Tobiáš Tláskal
  */
+
+@Composable
+fun Message(){
+
+}
+
 
 @Composable
 fun Error(message: String?, onClick: () -> Unit){
@@ -35,8 +46,20 @@ fun Error(message: String?, onClick: () -> Unit){
             Text(
                 text = message,
                 modifier = Modifier.padding(5.dp),
-                color = Color.Black
+                color = Color.Black,
+                style = MaterialTheme.typography.bodyMedium
             )
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@PreviewLightDark
+@Composable
+fun ErrorPreview(){
+    InventurAppTheme {
+        Scaffold(Modifier.heightIn(30.dp, 60.dp)) {
+            Error("Test") {}
         }
     }
 }
--- app/src/main/java/cz/tlaskal/inventurapp/ui/item/ItemDeteailViewModel.kt
+++
@@ -1,7 +0,0 @@
-package cz.tlaskal.inventurapp.ui.item
-
-import androidx.lifecycle.ViewModel
-
-class ItemDeteailViewModel: ViewModel() {
-
-}
