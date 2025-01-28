2deb05e5ff3f3f6125d933785bca53b69bb2c514
tobias.tlaskal <tobias.tlaskal@getready.cz>
2025-01-28 22:40:15 +0100

    endOnLineBreak String Util created

--- app/src/main/java/cz/tlaskal/inventurapp/util/StringUtils.kt
+++ app/src/main/java/cz/tlaskal/inventurapp/util/StringUtils.kt
@@ -1,10 +1,24 @@
 package cz.tlaskal.inventurapp.util
 
 fun String.limitLength(maxLength: Int): String {
-    return if(this.count() < maxLength)
-    {
+    return if (this.count() < maxLength) {
         this
-    }else{
+    } else {
         this.substring(0, maxLength) + "..."
     }
+}
+
+fun String.endOnLineBreak(): String {
+    val eolIndex = this.indexOf('\n')
+    if (eolIndex != -1) {
+        val firstLine = this.substring(0, eolIndex)
+        if (
+            this.substring(eolIndex, this.count())
+                .isNotBlank()
+        ) {
+            return "$firstLine\n..."
+        }
+         return firstLine
+    }
+    return this
 }
