eb35abe6ceeeb422864cf5c51f4f3b0a1edceb93
tobias.tlaskal <tobias.tlaskal@getready.cz>
2025-01-28 22:41:55 +0100

    MainActivity.kt cleanup

--- app/src/main/java/cz/tlaskal/inventurapp/MainActivity.kt
+++ app/src/main/java/cz/tlaskal/inventurapp/MainActivity.kt
@@ -1,34 +1,15 @@
 package cz.tlaskal.inventurapp
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
-import cz.tlaskal.inventurapp.ui.components.ScannerTextViewModel
-import cz.tlaskal.inventurapp.ui.theme.InventurAppTheme
 import cz.tlaskal.inventurapp.util.DatabaseSeeder
-import dagger.hilt.android.scopes.ActivityScoped
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.count
-import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.newCoroutineContext
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.coroutineContext
 
 class MainActivity : ComponentActivity() {
     val itemRepo = lazy { (application as InventurApplication).container.itemsRepository }
@@ -46,21 +27,5 @@ class MainActivity : ComponentActivity() {
         setContent {
                 InventurApp()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name! Scan the code below.",
-        modifier = modifier
-    )
-}
-
-
-@Composable
-fun GreetingPreview() {
-    InventurAppTheme {
-        Greeting("Android")
     }
 }
