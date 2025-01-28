34c9ee3e53f8b8e5a8e6fcd0da158b3c046452c7
tobias.tlaskal <tobias.tlaskal@getready.cz>
2025-01-28 22:39:56 +0100

    ScannerTextView -> ScannerTextField
    Velká změna ve state managementu - předěláno na hoisting pomocí lambda onValueChange, text se injektuje konstruktorem

--- app/src/main/java/cz/tlaskal/inventurapp/ui/components/ScannerTextField.kt
+++ app/src/main/java/cz/tlaskal/inventurapp/ui/components/ScannerTextField.kt
@@ -11,8 +11,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusTarget
@@ -21,49 +22,46 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import cz.tlaskal.inventurapp.R
 
 @Composable
-fun ScannerTextView(
-    viewModel: ScannerTextViewModel = viewModel(),
+fun ScannerTextField(
+    text: String?,
     labelText: String? = null,
     isError: Boolean = false,
     supportingText: String? = null,
-    onValueChange: (TextFieldValue) -> Unit = {}
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    onValueChange: (TextFieldValue) -> Unit = {},
 ) {
-    val uiState = viewModel.uiState.collectAsState()
+    val showScannerState: MutableState<Boolean> = remember { mutableStateOf(false) }
+
     val focusManager = LocalFocusManager.current
 
-    LaunchedEffect(uiState.value.barcode) {
-        onValueChange(uiState.value.barcode)
-    }
-
-
-    val supportingText: @Composable (() -> Unit)? = {
-        if (supportingText != null) {
-            Text(supportingText)
+    val supportingText = {
+        if (supportingText?.isNotBlank() == true) {
+            @Composable { Text(supportingText) }
+        } else {
+            null
         }
     }
 
-    if (uiState.value.showScanner) {
+    if (showScannerState.value && enabled) {
         BarcodeScanner {
-            viewModel.barcodeChanged(
-                TextFieldValue(
-                    text = it.rawValue.toString(),
-                    selection = TextRange(it.rawValue.toString().length)
-                )
+            val result = TextFieldValue(
+                text = it.rawValue.toString(),
+                selection = TextRange(it.rawValue.toString().length)
             )
+            onValueChange(result)
         }
-        viewModel.showScanner(false)
+        showScannerState.value = false
     }
 
     Row(verticalAlignment = Alignment.CenterVertically) {
         TextField(
-            value = uiState.value.barcode,
+            value = TextFieldValue(text ?: "", TextRange(text?.count()?: 0)),
             onValueChange = { it: TextFieldValue ->
-                viewModel.barcodeChanged(it)
+                onValueChange(it)
             },
             modifier = Modifier
                 .weight(7f)
@@ -71,16 +69,20 @@ fun ScannerTextView(
                 .focusTarget(),
             label = { Label(labelText) },
             trailingIcon = {
-                TrailingIcon(viewModel)
+                if (enabled && !readOnly) {
+                    TrailingIcon(text, showScannerState, onValueChange)
+                }
             },
             singleLine = true,
             isError = isError,
-            supportingText = supportingText,
+            supportingText = supportingText(),
             keyboardActions = KeyboardActions(
                 onDone = {
                     focusManager.clearFocus()
                 }
-            )
+            ),
+            enabled = enabled,
+            readOnly = readOnly
         )
     }
 }
@@ -94,17 +96,19 @@ private fun Label(label: String?) {
 }
 
 @Composable
-private fun TrailingIcon(viewModel: ScannerTextViewModel) {
-    val uiState = viewModel.uiState.collectAsStateWithLifecycle()
+private fun TrailingIcon(
+    text: String?,
+    showScannerState: MutableState<Boolean>,
+    onValueChange: (TextFieldValue) -> Unit
+) {
 
     Row {
-        if (uiState.value.barcode.text.isNotEmpty()) {
-
-            IconButton(onClick = { viewModel.barcodeChanged(TextFieldValue("")) }) {
+        if (text?.isNotEmpty() == true) {
+            IconButton(onClick = { onValueChange(TextFieldValue("")) }) {
                 Icon(Icons.Sharp.Clear, stringResource(R.string.clear))
             }
         }
-        IconButton(onClick = { viewModel.showScanner() }) {
+        IconButton(onClick = { showScannerState.value = true }) {
             Icon(
                 painterResource(R.drawable.baseline_qr_code_scanner_24),
                 stringResource(R.string.scan_button),
