2173a701a0b19cde43d46b36eacdfa700a4fd1ad
tobias.tlaskal <tobias.tlaskal@getready.cz>
2025-01-28 22:41:19 +0100

    NewItemScreen rework
    State si managuje Screen, ViewModel pouze upravuje uiState

--- app/src/main/java/cz/tlaskal/inventurapp/ui/item/NewItemScreen.kt
+++ app/src/main/java/cz/tlaskal/inventurapp/ui/item/NewItemScreen.kt
@@ -1,21 +1,22 @@
 package cz.tlaskal.inventurapp.ui.item
 
-import android.os.Build
-import androidx.annotation.RequiresApi
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -23,8 +24,10 @@ import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.focusTarget
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -33,32 +36,35 @@ import cz.tlaskal.inventurapp.TopAppBar
 import cz.tlaskal.inventurapp.ui.theme.InventurAppTheme
 import cz.tlaskal.inventurapp.R
 import cz.tlaskal.inventurapp.data.Item
-import cz.tlaskal.inventurapp.ui.components.ScannerTextView
-import cz.tlaskal.inventurapp.ui.components.ScannerTextViewModel
+import cz.tlaskal.inventurapp.ui.components.ScannerTextField
 import cz.tlaskal.inventurapp.util.timestampToString
 
 
-@RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewItemScreen(
     viewModel: NewItemViewModel = viewModel(factory = NewItemViewModel.Factory),
     onNavigateBack: () -> Unit
 ) {
-    val scannerViewModel: ScannerTextViewModel = viewModel()
-    val scannerUiState = scannerViewModel.uiState.collectAsStateWithLifecycle()
+
     val uiState = viewModel.uiState.collectAsStateWithLifecycle()
     val idNotUniqueError = uiState.value.idNotUnique
     val errorMessage = uiState.value.error
 
-    val id = scannerUiState.value.barcode
-    var name = rememberSaveable { mutableStateOf("") }
+    val id = remember { mutableStateOf("") }
+    var name = remember{ mutableStateOf("") }
     var description = remember { mutableStateOf("") }
-    viewModel.checkId(scannerUiState)
 
     var showDatePicker = remember { mutableStateOf(false) }
     val datePickerState = rememberDatePickerState(System.currentTimeMillis())
-    var selectedDate = datePickerState.selectedDateMillis!!
+
+    val nameSupportText = {
+        if (uiState.value.nameIsBlank) {
+            @Composable { Text(stringResource(R.string.name_is_blank)) }
+        } else null
+    }
+
+    val focusRequester = FocusRequester()
 
     if (uiState.value.inserted) {
         onNavigateBack()
@@ -72,27 +78,6 @@ fun NewItemScreen(
                     showBack = true,
                     onBackClicked = onNavigateBack
                 )
-            },
-            floatingActionButton = @Composable {
-                var enabled = !idNotUniqueError && id.text.isNotBlank()
-                if (enabled) {
-                    FloatingActionButton(
-                        onClick = {
-                            viewModel
-                                .createNewItem(
-                                    Item(
-                                        id.text,
-                                        name.value,
-                                        description.value,
-                                        selectedDate,
-                                        false
-                                    )
-                                )
-                        },
-                    ) {
-                        Icon(Icons.Default.Check, stringResource(R.string.save))
-                    }
-                }
             }
         ) {
             Column(
@@ -104,28 +89,41 @@ fun NewItemScreen(
                     )
                     .padding(horizontal = 50.dp)
             ) {
-                val isError = idNotUniqueError
+                val isError = idNotUniqueError && id.value.isNotBlank()
                 val supportingText = if (isError) stringResource(R.string.id_not_unique) else null
-                ScannerTextView(
-                    viewModel = scannerViewModel,
+                ScannerTextField(
+                    text = id.value,
                     labelText = stringResource(R.string.new_item_code),
                     isError = isError,
-                    supportingText = supportingText
+                    supportingText = supportingText,
+                    onValueChange = { id.value = it.text; viewModel.validateId(it.text) },
                 )
+
                 OutlinedTextField(
                     value = name.value,
                     label = @Composable { Text(stringResource(R.string.name)) },
-                    onValueChange = { name.value = it },
-                    singleLine = true
+                    onValueChange = {
+                        name.value = it
+                        viewModel.nameChanged(it)
+                    },
+                    isError = uiState.value.nameIsBlank,
+                    supportingText = nameSupportText(),
+                    singleLine = true,
+                    modifier = Modifier
+                        .focusTarget()
+                        .focusRequester(focusRequester)
+                        .focusable(uiState.value.nameIsBlank)
                 )
                 OutlinedTextField(
                     value = description.value,
                     label = @Composable { Text(stringResource(R.string.description)) },
                     onValueChange = { description.value = it },
-                    minLines = 3
+                    minLines = 3,
+                    maxLines = 7,
+                    modifier = Modifier.focusTarget()
                 )
                 OutlinedTextField(
-                    value = timestampToString(selectedDate),
+                    value = timestampToString(datePickerState.selectedDateMillis!!),
                     onValueChange = { },
                     label = @Composable { Text(stringResource(R.string.created_at)) },
                     readOnly = true,
@@ -140,11 +138,36 @@ fun NewItemScreen(
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(64.dp)
+                        .focusTarget()
                 )
+                Spacer(Modifier.height(50.dp))
+                Button(
+                    onClick = {
+                        viewModel
+                            .createNewItem(
+                                Item(
+                                    id.value,
+                                    name.value,
+                                    description.value,
+                                    datePickerState.selectedDateMillis!!,
+                                    false
+                                )
+                            )
+                        focusRequester.requestFocus()
+                    },
+                    enabled = !uiState.value.idNotUnique,
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = OutlinedTextFieldDefaults.shape
+                )
+                {
+                    Text(stringResource(R.string.create_item))
+                }
+
 
                 if (showDatePicker.value) {
                     DatePickerModal(
-                        onDateSelected = { selectedDate = it!! },
+                        state = datePickerState,
+                        onDateSelected = { datePickerState.selectedDateMillis = it },
                         onDismiss = { showDatePicker.value = false }
                     )
                 }
@@ -157,16 +180,16 @@ fun NewItemScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DatePickerModal(
+    state: DatePickerState,
     onDateSelected: (Long?) -> Unit,
     onDismiss: () -> Unit
 ) {
-    val datePickerState = rememberDatePickerState()
 
     DatePickerDialog(
         onDismissRequest = onDismiss,
         confirmButton = {
             TextButton(onClick = {
-                onDateSelected(datePickerState.selectedDateMillis)
+                onDateSelected(state.selectedDateMillis)
                 onDismiss()
             }) {
                 Text("OK")
@@ -178,6 +201,6 @@ fun DatePickerModal(
             }
         }
     ) {
-        DatePicker(state = datePickerState)
+        DatePicker(state = state)
     }
 }
--- app/src/main/java/cz/tlaskal/inventurapp/ui/item/NewItemViewModel.kt
+++ app/src/main/java/cz/tlaskal/inventurapp/ui/item/NewItemViewModel.kt
@@ -1,7 +1,6 @@
 package cz.tlaskal.inventurapp.ui.item
 
 import android.database.sqlite.SQLiteException
-import androidx.compose.runtime.State
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
@@ -11,30 +10,29 @@ import androidx.lifecycle.viewmodel.viewModelFactory
 import cz.tlaskal.inventurapp.InventurApplication
 import cz.tlaskal.inventurapp.data.Item
 import cz.tlaskal.inventurapp.data.ItemsRepository
-import cz.tlaskal.inventurapp.ui.components.ScannerTextViewModel
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 data class NewItemUiState(
     val inserted: Boolean = false,
-    val idNotUnique: Boolean = false,
-    val registeredIds: List<String> = emptyList(),
+    val idNotUnique: Boolean = true,
+    val nameIsBlank: Boolean = false,
     val error: String? = null
 )
 
 class NewItemViewModel(val itemsRepository: ItemsRepository) : ViewModel() {
     private val _uiState = MutableStateFlow(NewItemUiState())
     val uiState: StateFlow<NewItemUiState> = _uiState.asStateFlow()
+    var registeredIds: List<String> = emptyList()
+    private val idForValidation = MutableSharedFlow<String>()
 
 
-    companion object{
+    companion object {
         val Factory: ViewModelProvider.Factory = viewModelFactory {
             initializer {
                 val application = (this[APPLICATION_KEY] as InventurApplication)
@@ -45,38 +43,65 @@ class NewItemViewModel(val itemsRepository: ItemsRepository) : ViewModel() {
     }
 
     init {
+        registerItemIdsCollector()
+        registerIdValidator()
+    }
+
+    private fun registerItemIdsCollector() {
         viewModelScope.launch {
             itemsRepository.getAllItemsIdsStream().collect {
-                val registeredIdsList = it
-                _uiState.update { it.copy(registeredIds = registeredIdsList) }
+                registeredIds = it
             }
         }
     }
 
-    fun createNewItem(item: Item){
-        if(!uiState.value.idNotUnique){
-            viewModelScope.launch{
+    @OptIn(FlowPreview::class)
+    private fun registerIdValidator() {
+        viewModelScope.launch {
+            idForValidation.collect {
+                if (
+                    it in registeredIds || it.isBlank()
+                ) {
+                    _uiState.update { it.copy(idNotUnique = true) }
+                } else if (uiState.value.idNotUnique == true) {
+                    _uiState.update { it.copy(idNotUnique = false) }
+                }
+            }
+        }
+    }
+
+    fun createNewItem(item: Item) {
+        if (item.nazev.isBlank()) {
+            _uiState.update { it.copy(nameIsBlank = true) }
+            return
+        }
+        if (!uiState.value.idNotUnique) {
+            viewModelScope.launch {
                 try {
                     itemsRepository.insertItem(item)
                     _uiState.update { it.copy(inserted = true) }
-                }catch (exception: SQLiteException){
+                } catch (exception: SQLiteException) {
                     _uiState.update { it.copy(error = exception.message) }
                 }
             }
         }
     }
 
-    @OptIn(FlowPreview::class)
-    fun checkId(scannerState: State<ScannerTextViewModel.ScannerUiState>){
-        var scannerInputFlow = flowOf(scannerState)
-        viewModelScope.launch{
-            scannerInputFlow.debounce(500).collectLatest {
-                if(it.value.barcode.text in uiState.value.registeredIds){
-                    _uiState.update { it.copy(idNotUnique = true) }
-                }else if (uiState.value.idNotUnique == true){
-                    _uiState.update { it.copy(idNotUnique = false) }
-                }
-            }
+    fun validateId(id: String) {
+        viewModelScope.launch {
+            idForValidation.emit(id)
+        }
+    }
+
+    fun canCreateNewItem(): Boolean {
+        if (!uiState.value.idNotUnique) return true else return false
+    }
+
+    fun nameChanged(name: String) {
+        if (uiState.value.nameIsBlank && name.isNotBlank()) {
+            _uiState.update { it.copy(nameIsBlank = false) }
+        } else if (name.isBlank()) {
+            _uiState.update { it.copy(nameIsBlank = true) }
         }
     }
 }
