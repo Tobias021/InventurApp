5ba69e1facc2afbbd6fe056c497a46be5b03f183
tobias.tlaskal <tobias.tlaskal@getready.cz>
2025-02-02 02:07:14 +0100

    InventoryCheck nav destination

--- app/src/main/java/cz/tlaskal/inventurapp/ui/nav/AppNavGraph.kt
+++ app/src/main/java/cz/tlaskal/inventurapp/ui/nav/AppNavGraph.kt
@@ -1,7 +1,5 @@
 package cz.tlaskal.inventurapp.ui.nav
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -9,8 +7,8 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
 import cz.tlaskal.inventurapp.ui.home.HomeScreen
+import cz.tlaskal.inventurapp.ui.inventorycheck.InventoryCheckScreen
 import cz.tlaskal.inventurapp.ui.item.ItemDetailScreen
-import cz.tlaskal.inventurapp.ui.item.ItemDetailUiState
 import cz.tlaskal.inventurapp.ui.item.NewItemScreen
 import kotlinx.serialization.Serializable
 
@@ -24,30 +22,34 @@ data class ItemDetail(val id: String)
 @Serializable
 object NewItem
 
+@Serializable
+object InventoryCheck
 
 
 @Composable
-fun AppNavHost(navController: NavHostController, modifier: Modifier = Modifier)
-{
+fun AppNavHost(navController: NavHostController, modifier: Modifier = Modifier) {
     NavHost(
         navController = navController,
         startDestination = Home,
         modifier = modifier,
-    ){
-        composable<Home>{
+    ) {
+        composable<Home> {
             HomeScreen(
-                onAddItem = {navController.navigate(NewItem)},
-                onEditItem = {
-                    navController.navigate(ItemDetail(it))
-                }
+                onAddItem = { navController.navigate(NewItem) },
+                onEditItem = { navController.navigate(ItemDetail(it)) },
+                onCheckItems = { navController.navigate(InventoryCheck) }
             )
         }
-        composable<NewItem>{
+        composable<NewItem> {
             NewItemScreen(onNavigateBack = { navController.popBackStack() })
         }
-        composable<ItemDetail>{
+        composable<ItemDetail> {
             val itemDetail: ItemDetail = it.toRoute()
-            ItemDetailScreen(id = itemDetail.id, onBackClicked = { navController.navigate(Home) })
+            ItemDetailScreen(id = itemDetail.id, onBackClicked = { navController.popBackStack() })
         }
+        composable<InventoryCheck> {
+            InventoryCheckScreen(onBackClicked = { navController.popBackStack() })
+        }
+
     }
 }
